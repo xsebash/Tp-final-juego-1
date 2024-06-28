864fbf0c6f3683a82259802ed198366f77913a30
SophieSued <163852514+SophieSued@users.noreply.github.com>
2024-06-28 10:42:28 -0300

    cambios

--- Assets/Scripts/ComportamientoJuego.cs
+++ Assets/Scripts/ComportamientoJuego.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ComportamientoJuego : MonoBehaviour
 {
     public Producto[] primerproducto;
     public Producto[] segundoproducto;
+    public Text Precio1;
+    public Text Precio2;
 
     void Comienzodejuego()
 
     {
+        DeactivateAll();
         Producto producto1rndm = primerproducto[UnityEngine.Random.Range(0, primerproducto.Length - 1)];
         Producto producto2rndm = segundoproducto[UnityEngine.Random.Range(0, segundoproducto.Length - 1)];
-        DeactivateAll();
+        producto1rndm.gameObject.SetActive(true);
+        producto2rndm.gameObject.SetActive(true);
     }
 
     void DeactivateAll()
@@ -33,7 +38,8 @@ public class ComportamientoJuego : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        DeactivateAll();
+        Comienzodejuego();
     }
 
     // Update is called once per frame
